3550b7247f52070d93498543ea71355412a915b4
BOBUS2317 <145115035+BOBUS2317@users.noreply.github.com>
2023-10-07 16:00:25 +0700

    Add files via upload

---
+++ task12.py
@@ -0,0 +1,12 @@
+def f12():
+    spisok=[]
+    for num in range(2,1000):
+        if all(num%delit!=0 for delit in range(2,num)):
+            spisok.append(num)
+            
+    for y in range (100):
+        if y*4+117 in spisok:
+            print(y)
+            break
+
+f12()

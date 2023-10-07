b5e4ec330ee1e3e5d642ae68cc669777b6098cde
BOBUS2317 <145115035+BOBUS2317@users.noreply.github.com>
2023-10-07 16:09:22 +0700

    Update task12.py

--- task12.py
+++ task12.py
@@ -1,12 +1,11 @@
 def f12():
-    spisok=[]
-    for num in range(2,1000):
-        if all(num%delit!=0 for delit in range(2,num)):
-            spisok.append(num)
-            
-    for y in range (100):
-        if y*4+117 in spisok:
-            print(y)
-            break
+    for n in range(4,10000):
+        s = "5" + n*"2"
+        while "52" in s or "2222" in s or "1122" in s:
+            s = s.replace("52", "11", 1)
+            s = s.replace("2222", "5", 1)
+            s = s.replace("1122", "25", 1)
+        if sum(map(int, s)) == 64:
+            print(n)
 
 f12()

a4a10808fade7cc900bfd013e211a37279851128
BOBUS2317 <145115035+BOBUS2317@users.noreply.github.com>
2023-10-07 15:25:47 +0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,7 +1,9 @@
-print('x y z w')
+def f2('x y z w')
 for x in range(2):
    for y in range(2):
       for z in range(2):
          for w in range(2):
             if (not(y<=x) or (z<=w) or not(z))==False:
                print(x, y, z, w)
+
+f2(1,2,3,4)

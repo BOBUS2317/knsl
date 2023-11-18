76ec7b6643be3d96a9cb3190374706b153a89e1c
BOBUS2317 <145115035+BOBUS2317@users.noreply.github.com>
2023-11-18 15:49:48 +0700

    Update task27A.py

--- task27A.py
+++ task27A.py
@@ -1,8 +1,10 @@
-f = open('27A.txt')
+f = open('27A_2724.txt')
 n = int(f.readline())
 k = [0]*131
 for i in range(n):
-× = int(f.readline()) k[x%131]+=1
-count =
-count += k[0]*(k[0]-1)//2 for i in range(1,65+1): count+=k[i]*k[131-i]
+  × = int(f.readline()) 
+  k[x%131]+=1
+count = 0
+count += k[0]*(k[0]-1)//2
+for i in range(1,65+1): count+=k[i]*k[131-i]
 print(count)

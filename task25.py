d380fd1e8ece7022e5fd7331a2e9772d63cc4bea
BOBUS2317 <145115035+BOBUS2317@users.noreply.github.com>
2023-12-16 15:09:29 +0700

    Update task25.py

--- task25.py
+++ task25.py
@@ -1,7 +1,6 @@
 def f25():
-    for i in range(2023,10**10,2023):
+    for i in range(2024,10**10,2024):
         num=str(i)
-        if num[0]=='1' and num[2:6]=='2139' and num[-1]=='4':
-            print(i,i//2023)
-
+        if fnmatch.fnmatch(num, "1?2157*4") == True:
+            print(i,i//2024)
 f25()

0f6584e2d45e5e1cb848a89f7df4bb5b5d21284b
BOBUS2317 <145115035+BOBUS2317@users.noreply.github.com>
2024-01-13 16:14:46 +0700

    Update task25.py

--- task25.py
+++ task25.py
@@ -1,6 +1,13 @@
-def f25():
+def f251():
     for i in range(2024,10**10,2024):
         num=str(i)
         if fnmatch.fnmatch(num, "1?2157*4") == True:
             print(i,i//2024)
-f25()
+f251()
+f252()
+import re
+def f25():
+    for i in range(2024,10**10,2024):
+        if re.match("^1.2157.*4$",str(i)):
+            print(i,i//2024)
+f252()
